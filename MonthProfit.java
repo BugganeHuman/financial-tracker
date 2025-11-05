2163e597e24f87d1c198f4ff8e73aa557ff3700e
BugganeHuman <markizdelakarabas@gmail.com>
2025-11-05 18:59:40 +0500

    Update MonthProfit.java

--- MonthProfit.java
+++ MonthProfit.java
@@ -4,6 +4,10 @@ public class MonthProfit {
     int year;
     int month;
     int profit;
-
+    public MonthProfit (int year,int month,int profit) {
+        this.year = year;
+        this.month = month;
+        this.profit = profit;
+    }
 
 }
