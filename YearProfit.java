65e3581d77c4df2226cf07f98d90b540dbd87c69
BugganeHuman <markizdelakarabas@gmail.com>
2025-11-05 18:58:00 +0500

    Add files via upload

---
+++ YearProfit.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class YearProfit {
+    int year;
+    int profit;
+
+    public YearProfit(int year, int profit) {
+        this.year = year;
+        this.profit = profit;
+    }
+
+    // IT WILL NEED TO REWRITE
+    @Override
+    public String toString() {
+        return "year " + year + " - " + "profit " + profit;
+    }
+
+
+
+}
